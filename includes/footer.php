12ea4530f0f7719371abee83d6c6ef278d1ebb49
izzetsalihkaraman-hash <fkaraman7406@gmail.com>
2025-12-17 01:36:39 +0300

    temizlik

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-</main> <footer class="py-3 mt-4 border-top">
-    <div class="container">
-        <p class="text-center text-body-secondary">© 2025 MotoShare, Inc. Tüm hakları saklıdır.</p>
-    </div>
-</footer>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
